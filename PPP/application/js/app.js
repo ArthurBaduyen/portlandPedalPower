79088cab128162bc17c53fe1446ded3ed76e3853
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-02-13 14:27:22 +0800

    editable rows

--- PPP/application/js/app.js
+++ PPP/application/js/app.js
@@ -1,6 +1,15 @@
 $("tr").click(function(){
-  // $("tr.action").remove();
+  $("tr.action").remove();
   $("tr.table-edit").removeClass("table-edit");
   $(this).addClass("table-edit");
   // $("<tr class='action'><td colspan='3'><button class='btn'>Delete</button><button class='btn'>Cancel</button><button class='btn'>Save</button></td></tr>").insertAfter(this); 
+  $("<tr class='action'><td colspan='3'><button class='btn'>Delete</button></td></tr>").insertAfter(this); 
+
 }) 
+
+
+$(document).on("click",".btn", function(){
+  console.log("button click")
+  $("tr.action").remove();
+  $("tr.table-edit").removeClass("table-edit");
+})
