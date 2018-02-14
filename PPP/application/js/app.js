4f6cb2e020fed86515e52f4e50d31914f33d2173
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-02-14 12:28:59 +0800

    js

--- PPP/application/js/app.js
+++ PPP/application/js/app.js
@@ -2,9 +2,7 @@ $("tr").click(function(){
   $("tr.action").remove();
   $("tr.table-edit").removeClass("table-edit");
   $(this).addClass("table-edit");
-  // $("<tr class='action'><td colspan='3'><button class='btn'>Delete</button><button class='btn'>Cancel</button><button class='btn'>Save</button></td></tr>").insertAfter(this); 
   $("<tr class='action'><td colspan='30'><button class='btn'>Delete</button></td></tr>").insertAfter(this); 
-
 }) 
 
 
