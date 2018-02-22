ca39668eb64d6e562d2ae07610bd37f31376bb70
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-02-22 10:37:12 +0800

    disable click on table header

--- application/js/app.js
+++ application/js/app.js
@@ -1,12 +1,17 @@
-$("tr").click(function(){
-  $("tr.action").remove();
-  $("tr.table-edit").removeClass("table-edit");
-  $(this).addClass("table-edit");
-  $("<tr class='action'><td colspan='30'><button class='btn'>Delete</button></td></tr>").insertAfter(this); 
+$("tr").on('click',function(){
+	// console.log($(this).parents().hasClass('.thead-default'))
+	if ($(this).parent('thead').prop('nodeName') == 'THEAD'){
+
+	} else {
+		$("tr.action").remove();
+		$("tr.table-edit").removeClass("table-edit");
+		$(this).addClass("table-edit");
+		$("<tr class='action'><td colspan='30'><button class='btn'>Delete</button></td></tr>").insertAfter(this);	
+	}
 }) 
 
 
-$(document).on("click",".btn", function(){
+$('.table').on("click",".btn", function(){
   $("tr.action").remove();
-  $("tr.table-edit").removeClass("table-edit");  
+  $("tr.table-edit").removeClass("table-edit");
 })
