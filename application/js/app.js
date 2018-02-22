e05cad3627130759c284fa259be46a2374e25403
Arthur Baduyen <arthurbaduyen@Arthur.local>
2018-02-22 10:56:17 +0800

    cancel button

--- application/js/app.js
+++ application/js/app.js
@@ -1,17 +1,23 @@
 $("tr").on('click',function(){
 	// console.log($(this).parents().hasClass('.thead-default'))
-	if ($(this).parent('thead').prop('nodeName') == 'THEAD'){
+	if ($(this).parent('thead').is('thead')){
 
 	} else {
 		$("tr.action").remove();
 		$("tr.table-edit").removeClass("table-edit");
 		$(this).addClass("table-edit");
-		$("<tr class='action'><td colspan='30'><button class='btn'>Delete</button></td></tr>").insertAfter(this);	
+		$("<tr class='action'><td colspan='30'><button class='btn btnDelete'>Delete</button> <button class='btn btnCancel'>Cancel</button></td></tr>").insertAfter(this);	
 	}
 }) 
 
 
-$('.table').on("click",".btn", function(){
-  $("tr.action").remove();
-  $("tr.table-edit").removeClass("table-edit");
+$('.table').on("click",".btnCancel", function(){
+	$("tr.action").remove();
+	$("tr.table-edit").removeClass("table-edit");
+})
+
+$('.table').on("click",".btnDelete", function(){
+	$(this).parents('tr').prev('tr').remove()
+  	$("tr.action").remove();
+  	$("tr.table-edit").removeClass("table-edit");
 })
